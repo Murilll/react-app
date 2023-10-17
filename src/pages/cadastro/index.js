adfd8bb906db9d95f821402653b062792e26237e
Murilll <murilosertorio25@gmail.com>
2023-10-17 16:41:20 -0300

    update: page register

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,112 +1,116 @@
 import { useState } from "react"
-import { Text, Button, Image, TextInput, View, Switch } from "react-native"
+import { Text, Button, Image, TextInput, View, Switch, TouchableOpacity, StyleSheet } from "react-native"
 
 export default function Register(props) {
     const [name, setName] = useState('');
+    const [idade, setIdade] = useState();
+    const [sexo, setSexo] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
     return (
-        <>
-            <Text>Cadastro</Text>
-            <Button onPress={() => props.navigation.navigate('login')} title="Cadastro"></Button>
+        <View style={styles.screen}>
+            <View style={styles.container}>
+                <Button onPress={() => props.navigation.navigate('login')} title="Cadastro"></Button>
 
-            <Image
-                style={{
-                    height: "200px",
-                    width: "200px"
-                }}
-                source={require("/assets/user.png")}
-            />
-
-            <View>
-                <Text>Nome:</Text>
-                <TextInput
-                    multiline
-                    maxLength={40}
-                    numberOfLines={1}
-                    onChangeText={text => setName(text)}
+                <Image
                     style={{
-                        border: "solid black 2px"
+                        height: "200px",
+                        width: "200px"
                     }}
+                    source={require("/assets/user.png")}
                 />
+
+                <View style={styles.input}>
+                    <Text>Nome:</Text>
+                    <TextInput
+                        multiline
+                        maxLength={40}
+                        numberOfLines={1}
+                        onChangeText={text => setName(text)}
+                        style={{
+                            border: "solid black 2px"
+                        }}
+                    />
+                </View>
+
+                <View style={styles.input2}>
+                    <View>
+                        <Text>Idade:</Text>
+                        <TextInput />
+                    </View>
+
+                    <View>
+                        <Text>Sexo:</Text>
+                        <TextInput />
+                    </View>
+                </View>
+
+                <View style={styles.input}>
+                    <Text>E-mail:</Text>
+                    <TextInput />
+                </View>
+
+                <View style={styles.input}>
+                    <Text>Senha:</Text>
+                    <TextInput />
+                </View>
+
+                <View style={styles.input}>
+                    <Text>Confirmar senha:</Text>
+                    <TextInput />
+                </View>
+
+                <Switch
+                    trackColor={{ false: "#767577", true: "#324535" }}
+                />
+
+                <View style={styles.input}>
+                    <Button
+                        color={"black"}
+                        title='Cadastrar'
+                    />
+
+                    <TouchableOpacity onPress={() => console.log("Touchable")}
+                        style={{
+                            width: '100%'
+                        }}
+                    >
+                        <Text>Cancelar</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <Text>{name}</Text>
             </View>
-
-            <View>
-                <Text>Idade:</Text>
-                <TextInput
-                    multiline
-                    maxLength={40}
-                    numberOfLines={1}
-                    style={{
-                        border: "solid black 2px"
-                    }}
-                />
-
-                <Text>Sexo:</Text>
-                <TextInput
-                    multiline
-                    maxLength={40}
-                    numberOfLines={1}
-                    style={{
-                        border: "solid black 2px"
-                    }}
-                />
-            </View>
-
-            <View>
-                <Text>E-mail:</Text>
-                <TextInput
-                    multiline
-                    maxLength={40}
-                    numberOfLines={1}
-                    style={{
-                        border: "solid black 2px"
-                    }}
-                />
-            </View>
-
-            <View>
-                <Text>Senha:</Text>
-                <TextInput
-                    multiline
-                    maxLength={40}
-                    numberOfLines={1}
-                    style={{
-                        border: "solid black 2px"
-                    }}
-                />
-            </View>
-
-            <View>
-                <Text>Confirmar senha:</Text>
-                <TextInput
-                    multiline
-                    maxLength={40}
-                    numberOfLines={1}
-                    style={{
-                        border: "solid black 2px"
-                    }}
-                />
-            </View>
-
-            <View>
-                <Text>Senha:</Text>
-                <TextInput
-                    multiline
-                    maxLength={40}
-                    numberOfLines={1}
-                    style={{
-                        border: "solid black 2px"
-                    }}
-                />
-            </View>
-
-            <Switch
-                trackColor={{ false: "#767577", true: "#324535" }}      
-            />
-
-            
-
-            <Text>{name}</Text>
-        </>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        width: '100vw',
+        height: '100%',
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    container: {
+        backgroundColor: '#EFEDEE',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '40vw',
+    },
+    input: {
+        marginBottom: '1vw',
+        width: '80%',
+        borderRadius: 2,
+        backgroundColor: 'white',
+    },
+    input2: {
+        marginBottom: '1vw',
+        width: '80%',
+        display: 'flex',
+        borderRadius: 2,
+        backgroundColor: 'white'
+    }
+});
